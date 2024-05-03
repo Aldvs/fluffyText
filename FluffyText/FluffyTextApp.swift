4716badeed3809f60e9df5ddaa8cab00d6bfa1dd
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-03 12:15:07 +0300

    Initial Commit

---
+++ FluffyText/FluffyTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluffyTextApp.swift
+//  FluffyText
+//
+//  Created by admin on 03.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct FluffyTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
