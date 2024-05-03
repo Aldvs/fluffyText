2b6eee41decd141ba6db5a359d0854fd0fda17ac
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-03 22:17:54 +0300

    Setup text and slider

--- FluffyText/ContentView.swift
+++ FluffyText/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var sliderValue: CGFloat = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                HStack {
+                    Text("Марафон")
+                    Text("по SwiftUI")
+                }
+                Text("\u{00AB}Отцовский пинок\u{00BB}")
+            }
+//            .padding()
+            .frame(width: sliderValue)
+            .border(.purple, width: 2)
+            Slider(
+                value: $sliderValue,
+                in: 100...300,
+                label: { Image(systemName: "paperplane.fill") },
+                minimumValueLabel: { Text("100") },
+                maximumValueLabel: { Text("300") }
+            )
+            .padding()
+            .border(.cyan, width: 2)
         }
-        .padding()
     }
 }
 
