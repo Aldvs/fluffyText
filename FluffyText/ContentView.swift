010f979ecbfa3db53b42459b21dd0036e131f46e
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-04 14:00:56 +0300

    Setup multi styled text with line break

--- FluffyText/ContentView.swift
+++ FluffyText/ContentView.swift
@@ -8,30 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var sliderValue: CGFloat = 0.0
+    @State var sliderValue: CGFloat = 300.0
+    @State var animateGradient: Bool = false
     
     var body: some View {
         VStack {
-            VStack {
-                HStack {
-                    Text("Марафон")
-                    Text("по SwiftUI")
-                }
-                Text("\u{00AB}Отцовский пинок\u{00BB}")
-            }
-//            .padding()
-            .frame(width: sliderValue)
-            .border(.purple, width: 2)
+            MultiStyledText()
+                .animation(.bouncy, value: sliderValue)
+                .frame(width: sliderValue, height: 200)
+                .border(.red, width: 2)
             Slider(
                 value: $sliderValue,
-                in: 100...300,
-                label: { Image(systemName: "paperplane.fill") },
-                minimumValueLabel: { Text("100") },
-                maximumValueLabel: { Text("300") }
+                in: 100...300
             )
+            .tint(.gray)
             .padding()
-            .border(.cyan, width: 2)
         }
+        .frame(height: 500)
+    }
+}
+
+struct MultiStyledText: View {
+    var body: some View {
+        Text("Марафон ")
+            .foregroundStyle(.secondary)
+        +
+        Text(" ")
+        +
+        Text("по SwiftUI")
+            .fontWeight(.medium)
+        +
+        Text(" ")
+        +
+        Text("\u{00AB}Отцовский пинок\u{00BB}")
+            .font(.title)
+            .bold()
+            .foregroundStyle(.tint)
     }
 }
 
